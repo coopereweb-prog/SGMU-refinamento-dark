3914dae644c9b707efbc1c859d3f6db8b3316d9a
[dyad] <coopereweb@gmail.com>
2025-10-10 16:03:09 -0300

    Reverted all changes back to version 5a07dc549a65feb4bd5c91e2a65e1aa975526262

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { AuthProvider } from './contexts/AuthContext';
 import { UserProvider } from './contexts/UserContext';
 import { MapConfigProvider } from './contexts/MapConfigContext';
 
-import { PublicLayout } from './components/PublicLayout';
 import { AppLayout } from './components/AppLayout';
 import { AdminLayout } from './components/admin/AdminLayout';
 import { ProtectedRoute } from './components/ProtectedRoute';
@@ -41,17 +40,13 @@ function App() {
         <MapConfigProvider>
           <Router>
             <Routes>
-              {/* Rotas Públicas com PublicLayout */}
-              <Route element={<PublicLayout />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/quem-somos" element={<QuemSomosPage />} />
-                <Route path="/nossos-servicos" element={<NossosServicosPage />} />
-                <Route path="/como-adquirir" element={<ComoAdquirirPage />} />
-                <Route path="/trabalhe-conosco" element={<TrabalheConoscoPage />} />
-                <Route path="/fale-conosco" element={<FaleConoscoPage />} />
-              </Route>
-
-              {/* Rotas de Autenticação sem layout */}
+              {/* Rotas Públicas */}
+              <Route path="/" element={<HomePage />} />
+              <Route path="/quem-somos" element={<AppLayout><QuemSomosPage /></AppLayout>} />
+              <Route path="/nossos-servicos" element={<AppLayout><NossosServicosPage /></AppLayout>} />
+              <Route path="/como-adquirir" element={<AppLayout><ComoAdquirirPage /></AppLayout>} />
+              <Route path="/trabalhe-conosco" element={<AppLayout><TrabalheConoscoPage /></AppLayout>} />
+              <Route path="/fale-conosco" element={<AppLayout><FaleConoscoPage /></AppLayout>} />
               <Route path="/login" element={<GuestRoute><LoginPage /></GuestRoute>} />
               <Route path="/signup" element={<GuestRoute><SignUpPage /></GuestRoute>} />
               <Route path="/update-password" element={<UpdatePasswordPage />} />
