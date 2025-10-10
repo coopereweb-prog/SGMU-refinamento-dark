c4fbd552ba072314712ed32a1450c7cd37f10861
[dyad] <coopereweb@gmail.com>
2025-10-10 16:10:20 -0300

    [dyad] Resolvendo aviso de acessibilidade em diálogo - wrote 1 file(s)

--- src/components/EditOrderDialog.jsx
+++ src/components/EditOrderDialog.jsx
@@ -59,7 +59,7 @@ export function EditOrderDialog({ order, isOpen, onClose, onSave }) {
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-[525px]">
+      <DialogContent className="sm:max-w-[525px]" aria-describedby={undefined}>
         <DialogHeader>
           <DialogTitle>Editar Pedido #{order.id.substring(0, 8)}</DialogTitle>
           <DialogDescription>
