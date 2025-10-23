639d29422d848a268b25e18f154941bdf12615e7
[dyad] <coopereweb@gmail.com>
2025-10-23 20:22:28 -0300

    [dyad] wrote 1 file(s)

--- src/components/PointForm.jsx
+++ src/components/PointForm.jsx
@@ -262,7 +262,7 @@ export function PointForm({ point, onSave, onCancel, allPoints = [] }) {
               <GoogleMap
                 mapContainerStyle={mapContainerStyle}
                 center={mapCenter}
-                zoom={19} {/* Zoom fixo em 19 para edição */}
+                zoom={19}
                 options={{ disableDefaultUI: true, zoomControl: true }}
               >
                 {/* Marcador do Ponto em Edição (Arrastável) */}
